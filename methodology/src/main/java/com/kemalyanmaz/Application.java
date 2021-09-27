47259d4c544729a0a6fd992f26fd3126134f45bb
KemalYanmaz <ulaskemalyanmaz@gmail.com>
2021-09-27 19:23:06 +0300

    A work for understanding the methodology of Spring Applications has added

---
+++ methodology/src/main/java/com/kemalyanmaz/Application.java
@@ -0,0 +1,22 @@
+package com.kemalyanmaz;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.kemalyanmaz.config.ApplicationConfig;
+import com.kemalyanmaz.service.OutputService;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+    	ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+    	OutputService outputService = context.getBean(OutputService.class);
+    	
+    	
+    	
+        for (int i=0;i<5;i++){
+            outputService.generateOutput("Kemal");
+            Thread.sleep(2000);
+        }
+    }
+}
