774e8b07a62a323b861f1cc52d2ef6af200605a8
KemalYanmaz <ulaskemalyanmaz@gmail.com>
2021-09-27 21:07:46 +0300

    A work about Profile changes has added

--- methodology/src/main/java/com/kemalyanmaz/Application.java
+++ methodology/src/main/java/com/kemalyanmaz/Application.java
@@ -11,9 +11,7 @@ public class Application {
     public static void main(String[] args) throws Exception {
     	ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
     	OutputService outputService = context.getBean(OutputService.class);
-    	
-    	
-    	
+    	    	
         for (int i=0;i<5;i++){
             outputService.generateOutput();
             Thread.sleep(2000);
